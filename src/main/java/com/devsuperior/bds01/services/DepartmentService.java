405a14693863749c9f0602ce98cad71c22312ed6
Miguel Morais <miguelmoraisdev@gmail.com>
2021-06-25 23:30:52 -0300

    Tests passing

---
+++ src/main/java/com/devsuperior/bds01/services/DepartmentService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.bds01.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.bds01.dto.DepartmentDTO;
+import com.devsuperior.bds01.entities.Department;
+import com.devsuperior.bds01.repositories.DepartmentRepository;
+
+@Service
+public class DepartmentService {
+	
+	@Autowired
+	private DepartmentRepository repository;
+	
+	public List<DepartmentDTO> findAll() {
+		List<Department> list = repository.findAll(Sort.by("name"));
+		List<DepartmentDTO> dto = list.stream().map(dep -> new DepartmentDTO(dep)).collect(Collectors.toList());
+		return dto;
+	}
+
+}
